52f45373be0d024ca976bf15750fdd12bd8c9f7b
Giulia Gallo <giulia.gallo2903@gmail.com>
2025-01-14 16:37:43 +0100

    ,

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        //chiedo l'intero all'utente
+        System.out.println("Inserisci un numero di elementi: ");
+        int N = sc.nextInt();
+        sc.nextLine();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//dichiaro e utilizzo l'arraylist
+        ArrayList<Integer> parole= new ArrayList<>();
+        HashSet<String> paroleDistinte = new HashSet<>();
+        HashSet<String> duplicati = new HashSet<>();
+        System.out.println("Inserisci " + N + " parole: ");
+        for (int i = 0; i < N; i++) {
+            System.out.println("Inserisci " + (i+1) + " parole: ");
+            String parola = sc.nextLine();
+            if (!paroleDistinte.add(parola)) {
+                duplicati.add(parola);
+
+            }
+
+
         }
+        // Passo 3: Stampare le parole duplicate
+        System.out.println("Parole duplicate: " + duplicati);
+
+        // Passo 4: Stampare il numero di parole distinte
+        System.out.println("Numero di parole distinte: " + paroleDistinte.size());
+
+        // Passo 5: Stampare l'elenco delle parole distinte
+        System.out.println("Elenco delle parole distinte: " + paroleDistinte);
+
+
+
     }
 }
